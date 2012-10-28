079ca882d112e7e56a2b714c09177b11f7282bd4
Arturo Fernandez <arturo@bsnux.com>
2012-10-28 21:45:20 +0100

    Replacing 'act' for 'action' in URL's

--- django_actions/urls.py
+++ django_actions/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('django_actions.views',
-    url(r'^act/(?P<app_n_model>[\w\.]+)/$', 'act', name='url_act'),
+    url(r'^action/(?P<app_n_model>[\w\.]+)/$', 'act', name='url_act'),
 )
