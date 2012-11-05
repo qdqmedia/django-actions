19d3a078a9e0876382b6c72ecdcd52381193addb
Arturo Fernandez <arturo@bsnux.com>
2012-11-05 10:55:03 +0100

    Adding a generic action to export queryset to CSV

---
+++ django_actions/actions.py
@@ -0,0 +1,22 @@
+from django.utils.translation import ugettext as _
+from django.http import HttpResponse
+
+import csv
+
+CSV_FILE = 'exported_data.csv'
+
+def export_csv_action(model, request, qset, **kwargs):
+    response = HttpResponse(mimetype='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="{0}"'.format(CSV_FILE)
+
+    writer = csv.writer(response)
+
+    for item in qset:
+        row = []
+        for var in vars(item):
+            row.append(item.__getattribute__(var))
+        writer.writerow(row)
+
+    return {'response': response}
+
+export_csv_action.short_description = _('export items to CSV')
