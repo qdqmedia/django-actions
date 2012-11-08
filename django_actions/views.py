92aedaaaff786205ae7d6cc1331f611b188e5d2b
Arturo Fernandez <arturo@bsnux.com>
2012-11-08 11:51:00 +0100

    Selecting all qs

--- django_actions/views.py
+++ django_actions/views.py
@@ -1,8 +1,37 @@
 # -*- coding: utf-8 -*-
+import pickle
+
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django_actions.forms import ActionForm
 from django.contrib.contenttypes.models import ContentType
+
+
+class ActionViewMixin(object):
+    def get_context_data(self, *args, **kwargs):
+        object_list_displayed = kwargs['object_list']
+        self.request.session['serialized_qs'] = pickle.dumps(object_list_displayed.query)
+        self.request.session['serialized_model_qs'] = object_list_displayed.model
+        actions = []
+        for action in self.actions:
+            if action.short_description:
+                actions.append(action.short_description)
+            else:
+                actions(action.__name__)
+        kwargs['actions'] = actions
+        return super(ActionViewMixin, self).get_context_data(**kwargs)
+
+    def post(self, request, *args, **kwargs):
+        if request.POST['action'] and request.POST['action'] != u'-1':
+            # Execute action passing values
+            action = request.POST['action']
+            # Building a empty queryset to load pickled data
+            model_class = request.session['serialized_model_qs']
+            new_qs = model_class.objects.all()[:1]
+            new_qs.query = pickle.loads(request.session['serialized_qs'])
+            response = self.actions[int(action)-1](new_qs)
+            return response
+        return HttpResponseRedirect('.')
 
 
 def act(request, app_n_model):
