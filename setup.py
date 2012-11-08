35bd8f1535641af2cedbc04433d1cea0bf62548f
Arturo Fernandez <arturo@bsnux.com>
2012-11-08 14:04:49 +0100

    Adding setup file for pip installation

---
+++ setup.py
@@ -0,0 +1,19 @@
+#-*- coding: utf-8 -*-
+from setuptools import setup
+import os
+
+CLASSIFIERS = []
+
+setup(
+    author="QDQ media",
+    author_email="dpto-tecnico@qdqmedia.com",
+    name='django-actions',
+    version='0.0.1',
+    description='',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    url='https://github.com/qdqmedia/django-actions/',
+    license='BSD License',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    zip_safe=False,
+)
