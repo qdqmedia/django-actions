278bac6645d1faa8b710f3a9587e83363db783b9
Arturo Fernandez <arturo@bsnux.com>
2012-11-12 16:39:25 +0100

    Checking if action is a tuple and executing lambda function

--- django_actions/views.py
+++ django_actions/views.py
@@ -13,12 +13,24 @@ class ActionViewMixin(object):
         self.request.session['serialized_model_qs'] = object_list_displayed.model
         # Selecting actions for <select> node
         actions = []
+        delete_actions = []
         for action in self.actions:
-            if action.short_description:
-                actions.append(action.short_description)
+            curr_action = action
+            # Only available under specific conditions
+            if isinstance(action, tuple):
+                if not action[0](self.request):
+                    delete_actions.append(curr_action)
+                    continue
+                else:
+                    curr_action = action[1]
+            if curr_action.short_description:
+                actions.append(curr_action.short_description)
             else:
-                actions(action.__name__)
+                actions(curr_action.__name__)
         kwargs['actions'] = actions
+        # Delete actions
+        for act in delete_actions:
+            self.actions.remove(act)
         return super(ActionViewMixin, self).get_context_data(**kwargs)
 
     def post(self, request, *args, **kwargs):
@@ -26,8 +38,8 @@ class ActionViewMixin(object):
         if request.POST['action'] and request.POST['action'] != u'-1':
             new_qs = None
             # Checking if we're going to use all qset or only selected items
-            if request.POST['select-across']:
-                if request.POST['select-across'] == u'0' and request.POST['action-select']:
+            if 'select-across' in request.POST and 'action-select' in request.POST:
+                if request.POST['select-across'] == u'0':
                     selected_ids = request.POST.getlist('action-select')
                     new_qs = model_class.objects.filter(pk__in=(selected_ids))
                 else:
@@ -37,5 +49,8 @@ class ActionViewMixin(object):
                     new_qs.query = pickle.loads(request.session['serialized_qs'])
                 # Execute action passing values
                 action = request.POST['action']
-                return self.actions[int(action)-1](qs=new_qs, request=request)
+                action_to_execute = self.actions[int(action)-1]
+                if isinstance(action_to_execute, tuple):
+                    return action_to_execute[1](qs=new_qs, request=request)
+                return action_to_execute(qs=new_qs, request=request)
         return HttpResponseRedirect('.')
