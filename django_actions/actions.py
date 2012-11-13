9c8863a4b3ff5411f6ba90f43c3b23a0ebefcf40
Giuseppe Ciotta <gciotta@gmail.com>
2012-11-13 20:39:24 +0100

    - apply security checks in POST - simplify context['action'] building phase - remove use of **kwargs in function signatures

--- django_actions/actions.py
+++ django_actions/actions.py
@@ -6,13 +6,13 @@ import csv
 CSV_FILE = 'exported_data.csv'
 
 
-def export_csv_action(**kwargs):
+def export_csv_action(request, queryset):
     response = HttpResponse(mimetype='text/csv')
     response['Content-Disposition'] = 'attachment; filename="{0}"'.format(CSV_FILE)
 
     writer = csv.writer(response)
 
-    for item in kwargs['qset']:
+    for item in queryset:
         row = []
         for var in vars(item):
             row.append(item.__getattribute__(var))
--- django_actions/views.py
+++ django_actions/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pickle
 
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseForbidden
 
 
 class ActionViewMixin(object):
@@ -11,46 +11,43 @@ class ActionViewMixin(object):
         # Storing serialized queryset using query attribute
         self.request.session['serialized_qs'] = pickle.dumps(object_list_displayed.query)
         self.request.session['serialized_model_qs'] = object_list_displayed.model
+
         # Selecting actions for <select> node
-        actions = []
-        delete_actions = []
+        available_actions = []
         for action in self.actions:
-            curr_action = action
             # Only available under specific conditions
-            if isinstance(action, tuple):
+            if isinstance(action, (tuple, list)):
                 if not action[0](self.request):
-                    delete_actions.append(curr_action)
                     continue
-                else:
-                    curr_action = action[1]
-            if curr_action.short_description:
-                actions.append(curr_action.short_description)
-            else:
-                actions(curr_action.__name__)
-        kwargs['actions'] = actions
-        # Delete actions
-        for act in delete_actions:
-            self.actions.remove(act)
+                action = action[1]
+            available_actions.append(getattr(action, 'short_description', action.__name__))
+        kwargs['actions'] = available_actions
         return super(ActionViewMixin, self).get_context_data(**kwargs)
 
     def post(self, request, *args, **kwargs):
         model_class = request.session['serialized_model_qs']
         if request.POST['action'] and request.POST['action'] != u'-1':
-            new_qs = None
             # Checking if we're going to use all qset or only selected items
             if 'select-across' in request.POST and 'action-select' in request.POST:
                 if request.POST['select-across'] == u'0':
-                    selected_ids = request.POST.getlist('action-select')
-                    new_qs = model_class.objects.filter(pk__in=(selected_ids))
+                    # select a specific set of items
+                    qs = model_class.objects.filter(pk__in=(request.POST.getlist('action-select')))
                 else:
                     # Building a empty queryset to load pickled data
-                    model_class = request.session['serialized_model_qs']
-                    new_qs = model_class.objects.all()[:1]
-                    new_qs.query = pickle.loads(request.session['serialized_qs'])
-                # Execute action passing values
-                action = request.POST['action']
-                action_to_execute = self.actions[int(action)-1]
-                if isinstance(action_to_execute, tuple):
-                    return action_to_execute[1](qs=new_qs, request=request)
-                return action_to_execute(qs=new_qs, request=request)
+                    qs = model_class.objects.all()[:1]
+                    qs.query = pickle.loads(request.session['serialized_qs'])
+
+                # Pick the submitted action
+                try:
+                    action_to_execute = self.actions[int(request.POST['action'])-1]
+                except (KeyError, IndexError, ValueError):
+                    return HttpResponseForbidden()
+
+                # Perform condition checks
+                if isinstance(action_to_execute, (tuple, list)):
+                    if not action[0](self.request):
+                        return HttpResponseForbidden()
+                    action_to_execute = action_to_execute[1]
+
+                return action_to_execute(request, qs)
         return HttpResponseRedirect('.')
