4a61d700eb867fb360bfa786223017aa4ea7aa4d
Martin Samsula <msamsula@qdqmedia.com>
2013-05-23 18:02:39 +0200

    Adding back whole qs count

--- django_actions/views.py
+++ django_actions/views.py
@@ -8,6 +8,7 @@ class ActionViewMixin(object):
     def get_context_data(self, *args, **kwargs):
         # Saves whole object list (without pagination) for eventual post
         kwargs['_whole_object_list'] = kwargs['object_list']
+        kwargs['all_items_count'] = kwargs['object_list'].count()
         # Actions available in templates
         descriptions = []
         for action in self.actions:
