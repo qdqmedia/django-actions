5b6450951ea7419a34b91c36aa533c3d4e453b75
Martin Samsula <msamsula@qdqmedia.com>
2014-10-03 10:12:35 +0200

    bugfix: typo in action example

--- django_actions/actions.py
+++ django_actions/actions.py
@@ -6,7 +6,7 @@ import csv
 CSV_FILE = 'exported_data.csv'
 
 
-def export_csv_action(request, queryset):
+def export_csv_action(view, queryset):
     response = HttpResponse(mimetype='text/csv')
     response['Content-Disposition'] = 'attachment; filename="{0}"'.format(CSV_FILE)
 
